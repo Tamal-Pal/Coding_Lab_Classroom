4d3ca565611edee765a5ddb5c9689fc919977e38
shayantan <shayantankabiraj@gmail.com>
2024-05-06 18:18:05 +0530

    input field, output field beautified, and run button created

--- src/components/Editor/InputOutput/InputOutput.jsx
+++ src/components/Editor/InputOutput/InputOutput.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './InputOutput.css'
 
 const InputOutput = ({ className }) => {
-    return <div className={`${className} io`}>
-        <div>Input</div>
-        <div>Output</div>
-    </div>
+
+    const inputRef = useRef()
+    const outputRef = useRef()
+
+    const submitInput = () => {
+        console.log(inputRef.current.textContent)
+    }
+
+    return (        
+        <div className={`${className} io`} style={{ display: 'flex', flexDirection: 'column' }}>
+            <div style={{ flex: '0 0 50%' }} className='input-parent'>
+                <span>Input</span>
+                <a className='run-button' onClick={submitInput}>
+                    <i className="bi bi-play-fill"></i>
+                </a>
+                <div ref={inputRef} className='input' contentEditable='true'></div>
+            </div>
+            <div className='output-parent' style={{ flex: '0 0 50%' }}>
+                <div>Output</div>
+                <div ref={outputRef} className='output'></div>
+            </div>
+        </div>
+    )
 }
 
 export default InputOutput
